ed79032ae85a26f946cd753ad5cf6c17f0e2b4f6
jacobfrench <jakefrench1984@gmail.com>
2020-05-22 23:37:18 -0700

    update from desktop

--- resource-server/src/main/java/com/project/resourceserver/model/Job.java
+++ resource-server/src/main/java/com/project/resourceserver/model/Job.java
@@ -1,11 +1,15 @@
 package com.project.resourceserver.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -18,6 +22,53 @@ public class Job {
     @Column(name = "id", length = 36)
     private String id;
 
+    @Column(name = "scheduled_date_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime scheduledDateTime;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private GeoProperty property;
+
+    /**
+     * @return String return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return GeoProperty return the property
+     */
+    public GeoProperty getProperty() {
+        return property;
+    }
+
+    /**
+     * @param property the property to set
+     */
+    public void setProperty(GeoProperty property) {
+        this.property = property;
+    }
+
+    /**
+     * @return LocalDateTime return the scheduledDateTime
+     */
+    public LocalDateTime getScheduledDateTime() {
+        return scheduledDateTime;
+    }
+
+    /**
+     * @param scheduledDateTime the scheduledDateTime to set
+     */
+    public void setScheduledDateTime(LocalDateTime scheduledDateTime) {
+        this.scheduledDateTime = scheduledDateTime;
+    }
+
 }
--- resource-server/src/main/java/com/project/resourceserver/model/ServiceReport.java
+++ resource-server/src/main/java/com/project/resourceserver/model/ServiceReport.java
@@ -1,5 +1,33 @@
 package com.project.resourceserver.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "service_report")
 public class ServiceReport {
-    
+
+    @Column(name = "scheduled_date_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime completedDateTime;
+
+    /**
+     * @return LocalDateTime return the completedDateTime
+     */
+    public LocalDateTime getCompletedDateTime() {
+        return completedDateTime;
+    }
+
+    /**
+     * @param completedDateTime the completedDateTime to set
+     */
+    public void setCompletedDateTime(LocalDateTime completedDateTime) {
+        this.completedDateTime = completedDateTime;
+    }
+
 }
