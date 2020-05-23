ff27bef0fd9bd2da476f9f4b985df7570070f4d1
jacobfrench <jakefrench1984@gmail.com>
2020-05-23 12:21:23 -0700

    added id to service report entity

--- resource-server/src/main/java/com/project/resourceserver/model/ServiceReport.java
+++ resource-server/src/main/java/com/project/resourceserver/model/ServiceReport.java
@@ -4,30 +4,56 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "service_report")
 public class ServiceReport {
 
-    @Column(name = "scheduled_date_time")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-    private LocalDateTime completedDateTime;
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "id", length = 36)
+  private String id;
 
-    /**
-     * @return LocalDateTime return the completedDateTime
-     */
-    public LocalDateTime getCompletedDateTime() {
-        return completedDateTime;
-    }
+  @Column(name = "scheduled_date_time")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime completedDateTime;
 
-    /**
-     * @param completedDateTime the completedDateTime to set
-     */
-    public void setCompletedDateTime(LocalDateTime completedDateTime) {
-        this.completedDateTime = completedDateTime;
-    }
+  public ServiceReport(){}
+
+  /**
+   * @return LocalDateTime return the completedDateTime
+   */
+  public LocalDateTime getCompletedDateTime() {
+    return completedDateTime;
+  }
+
+  /**
+   * @param completedDateTime the completedDateTime to set
+   */
+  public void setCompletedDateTime(LocalDateTime completedDateTime) {
+    this.completedDateTime = completedDateTime;
+  }
+
+  /**
+   * @return String return the id
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
 
 }
