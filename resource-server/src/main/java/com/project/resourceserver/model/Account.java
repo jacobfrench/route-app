7b196456a3cd4f372cd84f52503463b75f47bf24
jacobfrench <jakefrench1984@gmail.com>
2020-09-02 21:58:56 -0700

    update

--- resource-server/src/main/java/com/project/resourceserver/model/Account.java
+++ resource-server/src/main/java/com/project/resourceserver/model/Account.java
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import org.hibernate.annotations.GenericGenerator;
+
 @Entity
 public class Account {
 
