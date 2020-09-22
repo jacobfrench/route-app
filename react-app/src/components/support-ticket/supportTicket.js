5fcd02f8ba728631c43d691b58d665b86edb6af2
jacobfrench <jakefrench1984@gmail.com>
2020-09-21 17:53:47 -0700

    updated interface

--- react-app/src/components/support-ticket/supportTicket.js
+++ react-app/src/components/support-ticket/supportTicket.js
@@ -1,13 +1,25 @@
 import React, { Fragment, useEffect, useState } from "react";
 import Breadcrumb from "../common/breadcrumb";
-import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
+import {
+  Container,
+  Col,
+  Row,
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Button
+} from "reactstrap";
 import Select from "react-select";
+import ReactTable from "react-table";
 
 const SupportTicket = () => {
   const [activeTab, setActiveTab] = useState("1");
   const [searchByType, setSearchByType] = useState(null);
   const [customerSearchFirstName, setCustomerSearchFirstName] = useState(null);
   const options = [
+    { value: "aid", label: "Account ID" },
     { value: "name", label: "Name" },
     { value: "address", label: "Address" },
   ];
@@ -17,39 +29,95 @@ const SupportTicket = () => {
   }
 
   function updateSerchType(type) {
-    setSearchByType(type);
+    console.log(type.label);
+    setSearchByType(type.label);
   }
 
+  function renderSearchOptions() {
+    switch (searchByType) {
+      case "Name":
+        return (
+          <Row>
+            <Col><input placeholder={"First Name"} class="form-control" type="text" onChange={(fname) => handleTextChanged(fname)} /></Col>
+            <Col style={{paddingLeft:0}}><input placeholder={"Last Name"} class="form-control" type="text" onChange={(fname) => handleTextChanged(fname)} /> </Col>
+          </Row>
+        );
+      case "Account ID":
+        return (
+          <Row>
+            <Col>
+              <input
+                placeholder={"Account ID"}
+                class="form-control"
+                type="text"
+                onChange={(fname) => handleTextChanged(fname)}
+              />
+            </Col>
+          </Row>
+        );
+      case "Address":
+        return (
+          <Row>
+            <Col>
+              <input
+                placeholder={"Street"}
+                class="form-control"
+                type="text"
+                onChange={(fname) => handleTextChanged(fname)}
+              />
+            </Col>
+            <Col style={{paddingLeft:0}}>
+              <input
+                placeholder={"City"}
+                class="form-control"
+                type="text"
+                onChange={(fname) => handleTextChanged(fname)}
+              />
+            </Col>
+            <Col style={{paddingLeft:0, width: 20}}>
+              <input
+                placeholder={"State"}
+                class="form-control"
+                type="text"
+                onChange={(fname) => handleTextChanged(fname)}
+              />
+            </Col>
+            <Col style={{paddingLeft:0}}>
+              <input
+                placeholder={"Zip"}
+                class="form-control"
+                type="text"
+                onChange={(fname) => handleTextChanged(fname)}
+              />
+            </Col>
+          </Row>
+        );
+      default:
+        return null;
+    }
+  }
 
   return (
     <Fragment>
       <Breadcrumb title="Customers" parent="Home" />
+
       <div className="container-fluid">
-        <div className="row">
+
+        <div className="col-sm-12">
+
+          <Container style={{ margin: 10, paddingLeft: 0 }}>
+            <h6>Search By:</h6>
+            <Row>
+              <Col className={"col-sm-2"}><Select options={options} placeholder={"Search by..."} onChange={(type) => updateSerchType(type)} style={{ paddingRight: 10 }} /></Col>
+              {renderSearchOptions()}
+              <Col><button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button></Col>
+            </Row>
+            
+          </Container>
           <div className="col-sm-12">
-            <div className="col-sm-12">
-              <div className="card" style={{ marginTop: 10 }}>
-                <div className="card-header">
-                  <h5>Search</h5>
-                </div>
-                <div className="card-body" style={{ paddingLeft: 0 }}>
-                
-                  <div className="col-sm-2">
-                    <Select options={options} placeholder={"Search by..."} onChange={(type) => updateSerchType(type)}/>
-                  </div>
+            {/* <div className="col-sm-12"> */}
 
-                  {(searchByType === "Name") ?
-                    <span>
-                      <div className="col-sm-2">
-                        <input placeholder={"First Name"} class="form-control" type="text" onChange={(fname) => handleTextChanged(fname)}/>
-                      </div>
-                    </span>
-                    : null
-                  } 
-
-                </div>
-              </div>
-            </div>
+            {/* </div> */}
             <Nav tabs className="borderb-tab-primary">
               <NavItem className="nav nav-tabs" id="myTab" role="tablist">
                 <NavLink
@@ -77,7 +145,7 @@ const SupportTicket = () => {
               </NavItem>
             </Nav>
             <TabContent activeTab={activeTab}>
-              <TabPane tabId="1">tab 1 content</TabPane>
+              <TabPane tabId="1"></TabPane>
               <TabPane tabId="2">tab2 content</TabPane>
               <TabPane tabId="3">tab3 content</TabPane>
             </TabContent>
